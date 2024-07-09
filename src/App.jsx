cc168d2fde01591f86413f1253220fad4a0a2a52
Maxiflexy <onyemax247@gmail.com>
2024-07-09 22:03:42 +0100

    faq and footer componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import "./App.css";
+import Footer from "./components/NavAndFooter/Footer";
 import NavBar from "./components/NavAndFooter/NavBar";
 import BusinessTask from "./components/hompage/business-task/BusinessTask";
 import GetStarted from "./components/hompage/business-task/GetStarted";
+import FaqSection from "./components/hompage/faq-section/FaqSection";
 import Features from "./components/hompage/features-section/Features";
 import HereSection from "./components/hompage/hero-section/HeroSection";
 import Pricing from "./components/hompage/pricing/Pricing";
@@ -19,6 +21,8 @@ function App() {
       <GetStarted />
       <Testimonials />
       <Pricing />
+      <FaqSection />
+      <Footer />
     </div>
   );
 }
