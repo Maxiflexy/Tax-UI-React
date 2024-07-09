7f0202b2c68eca2db900594922a6f102887ef76d
Maxiflexy <onyemax247@gmail.com>
2024-07-09 22:11:13 +0100

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@
 import "./App.css";
 import Footer from "./components/NavAndFooter/Footer";
 import NavBar from "./components/NavAndFooter/NavBar";
-import BusinessTask from "./components/hompage/business-task/BusinessTask";
-import GetStarted from "./components/hompage/business-task/GetStarted";
-import FaqSection from "./components/hompage/faq-section/FaqSection";
-import Features from "./components/hompage/features-section/Features";
-import HereSection from "./components/hompage/hero-section/HeroSection";
-import Pricing from "./components/hompage/pricing/Pricing";
-import Testimonials from "./components/hompage/testimonies/Testimonials";
+import { HomePage } from "./components/hompage/HomePage";
 
 function App() {
 
   return (
     <div>
       <NavBar />
+      <HomePage />
       <Footer />
     </div>
   );
