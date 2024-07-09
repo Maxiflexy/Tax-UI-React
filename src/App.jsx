4e5c4d25b5c5c88bb411066a8886a01168e781ef
Maxiflexy <onyemax247@gmail.com>
2024-07-09 22:09:22 +0100

    added other componenets to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,6 @@ function App() {
   return (
     <div>
       <NavBar />
-      <HereSection />
-      <Features />
-      <BusinessTask />
-      <GetStarted />
-      <Testimonials />
-      <Pricing />
-      <FaqSection />
       <Footer />
     </div>
   );
